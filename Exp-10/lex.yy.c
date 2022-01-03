2f6d22b9f59979be2d1148919e671608c6541193
Aju Jacob <ajugjacob@gmail.com>
2022-01-03 15:17:36 +0530

    Added exp-10

--- Exp-10/lex.yy.c
+++ Exp-10/lex.yy.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,20 +361,19 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[15] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    5,    3,    4,    3,    0,    1,    0,    0,
-        0,    1,    2,    0
+        0,    0,    6,    4,    2,    3,    1,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -401,31 +400,29 @@ static const YY_CHAR yy_ec[256] =
 
 static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    2,    1,    1
+        1,    1,    1,    2
     } ;
 
-static const flex_int16_t yy_base[16] =
+static const flex_int16_t yy_base[11] =
     {   0,
-        0,    0,   13,   14,   14,    2,    6,    0,    0,    0,
-        8,    0,    0,   14,   10
+        0,    0,    6,    7,    7,    7,    0,    0,    7,    3
     } ;
 
-static const flex_int16_t yy_def[16] =
+static const flex_int16_t yy_def[11] =
     {   0,
-       14,    1,   14,   14,   14,   14,   14,   15,    7,    7,
-        7,   15,    7,    0,   14
+        9,    1,    9,    9,    9,    9,   10,   10,    0,    9
     } ;
 
-static const flex_int16_t yy_nxt[19] =
+static const flex_int16_t yy_nxt[12] =
     {   0,
-        4,    5,    4,    6,    7,    8,    9,   10,   11,    9,
-       12,   13,   14,    3,   14,   14,   14,   14
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
+        9
     } ;
 
-static const flex_int16_t yy_chk[19] =
+static const flex_int16_t yy_chk[12] =
     {   0,
-        1,    1,    1,    1,    6,    6,    7,    7,    7,    7,
-       15,   11,    3,   14,   14,   14,   14,   14
+        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
+        9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -442,11 +439,13 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "comments.l"
-#line 2 "comments.l"
+#line 1 "calculator.l"
+#line 2 "calculator.l"
 #include<stdio.h>
+#include "y.tab.h"
+extern int yylval;
+#line 447 "lex.yy.c"
 #line 448 "lex.yy.c"
-#line 449 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -663,9 +662,9 @@ YY_DECL
 		}
 
 	{
-#line 6 "comments.l"
+#line 8 "calculator.l"
 
-#line 668 "lex.yy.c"
+#line 667 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -692,13 +691,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 15 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 14 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -724,26 +723,31 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "comments.l"
-{fprintf(yyout,"");};
+#line 9 "calculator.l"
+{yylval=atoi(yytext);return NUMBER;}
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 8 "comments.l"
-{fprintf(yyout,"");};
+#line 10 "calculator.l"
+;
 	YY_BREAK
 case 3:
+/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 9 "comments.l"
-{ fprintf(yyout, "%s", yytext); }
+#line 11 "calculator.l"
+return 0;
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 10 "comments.l"
+#line 12 "calculator.l"
+return yytext[0];
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 13 "calculator.l"
 ECHO;
 	YY_BREAK
-#line 746 "lex.yy.c"
+#line 750 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1040,7 +1044,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 15 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1068,11 +1072,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 15 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 14);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1748,23 +1752,9 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "comments.l"
+#line 13 "calculator.l"
 
 
-int yywrap(){
+int yywrap() {
     return 1;
 }
-
-// driver code
-int main()
-{
-	extern FILE *yyin, *yyout;
-
-	yyin = fopen("Input.txt", "r");
-
-	yyout = fopen("Output.txt", "w");
-
-	yylex();
-	return 0;
-}
-
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - [x] Study about phases of Compiler
 - [x] Write a program to implement a lexical analyser
 - [x] Write a program to implement a DFA
-- [ ] Write a program to implement a NFA
+- [x] Write a program to implement a NFA
 - [x] Write a program to implement NFA to DFA
 - [x] Construct a recursive descent parser for an expression
 
@@ -12,5 +12,5 @@
 - [x] Implement intermediate code generation for simple expressions
 - [x] Write LEX program to identity the tokens like keywords, identifiers, numbers, operators and punctuation symbols
 - [x] Write a LEX program to remove single line and multi line comments
-- [ ] Implementation of calculator using LEX and YACC
+- [x] Implementation of calculator using LEX and YACC
 - [ ] Implement a YACC program to recognize strings of the form a^nb^n
